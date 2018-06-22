297e4b213eb364ddfd01585fb55c56872507cfd0
Simon Rojas <srojasv10@gmail.com>
2018-06-22 14:24:38 -0400

    Add files

--- public/main.js
+++ public/main.js
@@ -1,9 +1,22 @@
 console.log("Starting main script...");
-console.log("Rendering")
-sigma.parsers.json('https://raw.githubusercontent.com/sroj/graphy/develop/public/data.json', {
-    container: 'container',
-    settings: {
-        defaultNodeColor: '#ec5148'
+console.log("Rendering the graph");
+// sigma.parsers.json('https://raw.githubusercontent.com/sroj/graphy/develop/public/data.json', {
+//     container: 'container',
+//     settings: {
+//         defaultNodeColor: '#ec5148'
+//     }
+// });
+
+sigma.parsers.gexf('https://raw.githubusercontent.com/sroj/graphy/develop/src/les-miserables.gexf', {
+        container: 'container',
+        settings: {
+            defaultNodeColor: '#ec5148'
+        }
+    },
+    function (s) {
+        // This function will be executed when the
+        // graph is displayed, with "s" the related
+        // sigma instance.
     }
-});
+);
 console.log("Done, bye...");
